241cde960bc00d79de1d176913b103857fbafd80
Whiteburn2023 <erunda995@yandex.ru>
2024-10-18 23:41:15 +0500

    работает!

--- HW23/src/Main.java
+++ HW23/src/Main.java
@@ -1,4 +1,35 @@
+import java.util.Scanner;
+import java.util.TreeSet;
+
 public class Main {
+    private static TreeSet<String> strings = new TreeSet<>();
+    private static String regex = "(7|8)?[0-9]{10}";
+    //"(?:7|8)[0-9]{10}";
+
     public static void main(String[] args) {
-    }
+
+        while(true){
+            System.out.println("Введите номер: ");
+            String input = new Scanner(System.in).nextLine();
+            if (input.contains("печать")){
+                for (String num: strings){
+                    System.out.println(num);
+                }
+            } else {
+            String str = input.replaceAll("\\D", "");
+            if (str.startsWith("7")||str.startsWith("8")){
+                str = str.substring(1);
+            }
+            if (strings.contains(str)) {
+                System.out.println("Такой номер уже есть в базе");
+            } else if (str.matches(regex)) {
+                    strings.add(str);
+                    System.out.println("Номер успешно добавлен " + str);
+
+            } else {
+                    System.out.println("Неверный формат!");
+            }
+            }
+        }
+        }
 }
