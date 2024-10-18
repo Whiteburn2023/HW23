c844dd15f854fe7cc1cbd86d30f913986ec37d93
Whiteburn2023 <erunda995@yandex.ru>
2024-10-19 01:12:01 +0500

    ооп и все такое

--- HW23/src/Main.java
+++ HW23/src/Main.java
@@ -2,33 +2,49 @@ import java.util.Scanner;
 import java.util.TreeSet;
 
 public class Main {
+    public static final String FORMAT_NUM_REGEX = "(\\d{3})(\\d{3})(\\d{2})(\\d{2})";
     private static TreeSet<String> strings = new TreeSet<>();
-    private static String regex = "(7|8)?[0-9]{10}";
 
     public static void main(String[] args) {
 
         while(true){
             System.out.println("Введите номер: ");
             String input = new Scanner(System.in).nextLine();
-            if (input.contains("печать")){
-                for (String num: strings){
-                    System.out.println(num);
-                }
+            if (input.equals("печать")){
+                print();
             } else {
-            String str = input.replaceAll("\\D", "");
-            if (str.startsWith("7")||str.startsWith("8")){
-                str = str.substring(1);
+                checkFormatNumber(input);
             }
-            if (strings.contains(str)) {
-                System.out.println("Такой номер уже есть в базе");
-            } else if (str.matches(regex)) {
-                    strings.add(str);
-                    System.out.println("Номер успешно добавлен " + str);
+        }
+        }
+    private static void checkFormatNumber(String input) {
+        String str = input.replaceAll("\\D", "");
+        if (str.length() > 11 || str.length() < 9) {
+            System.out.println("Неверный формат!");
+            return;
+        } else if (str.startsWith("7")||str.startsWith("8")){
+            str = str.substring(1);
+        }
+        //String regex = "(7|8)?[0-9]{10}";
+        add(str);
 
-            } else {
-                    System.out.println("Неверный формат!");
-            }
-            }
+    }
+    private static void add(String str){
+        if (strings.contains(str)) {
+            System.out.println("Такой номер уже есть в базе");
+            return;
         }
+        strings.add(str);
+        System.out.println("Номер успешно добавлен " + str);
+    }
+    private static void print(){
+        if (strings.isEmpty()) {
+            System.out.println("Список пуст!");
+            return;
         }
+        for (String num: strings){
+            String formatedNum = num.replaceAll(FORMAT_NUM_REGEX, "+7($1) $2 $3-$4");
+            System.out.println(formatedNum);
+        }
+    }
 }
